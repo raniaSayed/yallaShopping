d70a01e465e8226be5809cb7050ad1b884ccf65e
Menna Anees <mennaanees193@gmail.com>
2018-03-21 17:59:02 +0200

    product model

---
+++ NodeJs/models/products.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose");
+
+//seller ..............
+var Schema = mongoose.Schema;
+
+var products = new Schema(
+{
+  //auto increment id
+  // _id:{
+  name:{
+    type:String,
+    required:true
+  },
+  desc:{
+    type:String
+  },
+  price:{
+    type:Number
+  },
+  rate:{
+    type:Number
+  },
+  stock:{
+    type:Number
+  },
+  seller_id:{
+    type:Number,
+    unique: true,
+    required:true,
+    ref:"sellers"
+  },
+  subCategory_name:{
+    type:String,
+    required:true,
+    ref:"categories"
+  }
+}) ;
+products.plugin(autoIncrement.plugin, 'products');
+module.exports = mongoose.model("products",products);
