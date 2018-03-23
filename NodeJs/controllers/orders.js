ac48de579528b5a9505debd968b44ec5c19b8149
Mohamed Gamal <m.gemyero91@gmail.com>
2018-03-23 18:19:07 +0200

    make some enhancements in app.js and models

--- NodeJs/controllers/orders.js
+++ NodeJs/controllers/orders.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var JSONParsermid = bodyParser.json();
-var urlEncodedParsermid = bodyParser.urlencoded();
+var urlEncodedParsermid = bodyParser.urlencoded({extended: true});
 var router = express.Router();
 //var fs = require("fs");
 var mongoose = require("mongoose");
