1c0526aadcd8ae28088849802af75a0adabcc917
raniaSayed <raniasayed313@gmail.com>
2018-03-30 13:36:02 +0200

    finish filter products by price

--- Angular/src/app/search-product/search-product.component.ts
+++ Angular/src/app/search-product/search-product.component.ts
@@ -1,83 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductDetailsService } from '../services/product-details.service';
 import { CategoryService } from '../services/category.service';
- 
+
 @Component({
   selector: 'app-search-product',
   templateUrl: './search-product.component.html',
   styleUrls: ['./search-product.component.css']
 })
-export class SearchProductComponent implements OnInit {
-  searchWord:string;
+export class SearchProductComponent  {
+  searchWord: string;
   matchedProducts;
   categories;
-  categoryNames={};
-  selectedCategories =[];
-  saveUsername:boolean;
-  low =1;
-  high= 10000;
+  categoryNames = {};
+  selectedCategories = [];
+  saveUsername: boolean;
+  low = 1;
+  high = 10000;
 
-  constructor(private productDetailsService:ProductDetailsService,
-              private categoryService:CategoryService) {
+  constructor(private productDetailsService: ProductDetailsService,
+    private categoryService: CategoryService) {
     //get matched search products
-    console.log("1");
-    console.log(this.productDetailsService.myMethodSubject.getValue());
+    //console.log(this.productDetailsService.myMethodSubject.getValue());
     this.productDetailsService.myMethod$.subscribe(res => {
-      console.log("2");
 
-      productDetailsService.getMatchedProductData(res).subscribe(data => 
-        {
-          this.matchedProducts = data;
-          console.log("Matched");
-          console.log(this.matchedProducts);
-        });
+      productDetailsService.getMatchedProductData(res).subscribe(data => {
+        this.matchedProducts = data;
+      });
     });
 
     //get all categories and subcategories
-    this.categoryService.getAllCategoreis().subscribe(res =>{
+    this.categoryService.getAllCategoreis().subscribe(res => {
       this.categories = res;
       this.categories.forEach(element => {
         this.categoryNames[element] = false;
       });
 
     });
-   }
-   change(subcategory)
-{ 
+  }
+  change(subcategory) {
 
-  //change state  
-  this.categoryNames[subcategory] = !this.categoryNames[subcategory];
-          
-  //send request to get products
-  
-  this.selectedCategories=[];
+    //change state  
+    this.categoryNames[subcategory] = !this.categoryNames[subcategory];
+
+    //send request to get products
+
+    this.selectedCategories = [];
 
 
     //insert selected subcategory names
     Object.keys(this.categoryNames)
-    .filter(i => this.categoryNames[i]==true?this.selectedCategories.push(i) :"");
-    console.log(this.low);
-    console.log(this.high);
-    
-    this.productDetailsService.getFilteredProductData(this.low,this.high,this.selectedCategories)
-    .subscribe(res=> console.log(res));
-    
-}
-highChange(){
-  console.log("highChange");
-  
-  this.productDetailsService.getFilteredProductData(this.low,this.high,this.selectedCategories)
-    .subscribe(res=> console.log(res));
-    
-}
-lowChange(){
-  console.log("lowChange");
+      .filter(i => this.categoryNames[i] == true ? this.selectedCategories.push(i) : "");
 
-  this.productDetailsService.getFilteredProductData(this.low,this.high,this.selectedCategories)
-    .subscribe(res=> console.log(res));
-    
-}
-  ngOnInit() {
+    this.matchedProducts = [];
+    this.productDetailsService.getFilteredProductData(this.low, this.high, this.selectedCategories)
+      .subscribe(res => { this.matchedProducts = res });
+
   }
+  highChange() {
+    console.log("highChange");
+    this.matchedProducts = [];
 
+
+    this.productDetailsService.getFilteredProductData(this.low, this.high, this.selectedCategories)
+      .subscribe(res => { this.matchedProducts = res });
+
+  }
+  lowChange() {
+    this.matchedProducts = [];
+
+    this.productDetailsService.getFilteredProductData(this.low, this.high, this.selectedCategories)
+      .subscribe(res => { this.matchedProducts = res });
+
+  }
 }
